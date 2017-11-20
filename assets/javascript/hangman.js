57470fe261562fabed2ebe0fc6e583575374c631
Ovie <you@example.com>
2017-11-20 10:03:16 -0500

    adds some pseudo code

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,13 +1,30 @@
 //game logic
+    $(document).ready(function() {
 
 //press any key to get started
+
+var audioElement = document.createElement("audio");
+      audioElement.setAttribute("src", "./assets/GoTtheme.mp3");
+
+      // Theme Button
+      $(".theme-button").on("click", function() {
+        audioElement.play();
+      });
+
+      $(".pause-button").on("click", function() {
+        audioElement.pause();
+      });
+
+
 var guessesLeft=10;
 var wins=0;
 var losses=0;
+console.log("You have "+guessesLeft+" guesses left");
 
 var words = ["kaladin", "sanderson", "arya", "khaleesi"];
 
 //get a random word from array of options currentWord
+
 
 var currentWord = words[Math.floor(Math.random() * words.length)]
 console.log("current word is " + currentWord);
@@ -24,59 +41,57 @@ var hideWord = function() {
     for (var i = 0; i < wordString.length; i++) {
         hiddenLetters.push("_");
         console.log(hiddenLetters);
+        document.getElementById("hiddenWord").innerHTML = hiddenLetters.join(' ');
     }
 }
-debugger;
+
 hideWord();
+
+
+
 //hide the words in html?
 console.log($("#hiddenWord"));
-//$("#hiddenWord").html("");
-
-//$("#hiddenWord").innerHTML(hiddenLetters);
+//$("#hiddenWord").html("-");
 
 //get user input with onkeyup and compare input with wordString letters
 
 document.onkeyup = function(event) {
     var userKeyPress = event.key;
+    $("#lettersGuessed").append(" "+userKeyPress);
     var foundIt = false;
-    for (var i = 0; i < wordString.length; i++)
+    guessesLeft = guessesLeft-1;
+    	document.getElementById("guessesLeft").innerHTML = "You have "+guessesLeft+" guesses left!";
+    	console.log(guessesLeft);
+    for (var i = 0; i < wordString.length; i++){
         if (userKeyPress === wordString[i]) {
-            // alert("Well Done");
-            //display wordString[i];
             foundIt = true;
+            alert("You guessed a letter!");
             hiddenLetters[i] = userKeyPress;
+            document.getElementById("hiddenWord").innerHTML = hiddenLetters.join(' ');
            
         }
+        /*check if there are any more hidden letters and increase wins if not
+        for (var i = 0; i < hiddenLetters.length; i++) {
+        	if (hiddenLetters[i] != "-"){
+        		alert ("Winter Came");*/
+
+        	}
+        }
         
-    else {
-
-    	//make guesses--
-        // alert("Miss");
-        var guessesLeft = guessesLeft-1;
-    }
-    if (foundIt){
-    	alert("You guessd a letter!")
-    }
-    document.getElementById("word").innerHTML = hiddenLetters.join(' ');
-    console.log(hiddenLetters);
-
-}
-
-
-
-
+    
+    
+    
 
 
 
 //function to increase wins and increase losses
-//var playerWins= function(){
-//	wins = wins++;
-//}
 
-//var playerLoss = function(){
-//	losses = losses++;
-//}
-
+if(guessesLeft===0){
+	alert("You suck Joffrey!!");
+	var losses = losses +1;
+	document.getElementById("losses").innerHTML = losses;
+	console.log("you have "+losses+" losses")
+}
 
 
 
@@ -92,3 +107,6 @@ document.onkeyup = function(event) {
 	$("#hiddenWord").append("<span>_</span>")
 
 }*/
+
+
+})
